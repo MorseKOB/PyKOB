84739a57b0f1f85c89ebf0e432aca28bc2057ed0
Patrick Dirks <pdirks@vmware.com>
2020-04-29 21:48:21 -0700

    Fixed minute format in initial time announcement to be zero-filled to 2 digits (and took care of ^C handling, too)

--- Clock.py
+++ Clock.py
@@ -33,53 +33,58 @@ Serial port, code speed, and audio preferences should be specified by running th
 'configure.sh' script or executing 'python3 Configure.py'.
 """
 
-import time
-from pykob import config, kob, morse, log
-
-log.log('Starting Clock')
-
-PORT = config.Port # serial port for KOB interface
-WPM = config.Speed  # code speed (words per minute)
-SOUND = config.Sound # whether to enable computer sound for sounder
-MSGS = [
-    ( 900, 'The time is nine oclock     L  L  L  L  L  L  L  L  L'),
-    (1000, 'The time is ten oclock     L  L  L  L  L  L  L  L  L  L'),
-    (1100, 'The time is eleven oclock     L  L  L  L  L  L  L  L  L  L  L'),
-    (1200, 'The time is twelve oclock     L  L  L  L  L  L  L  L  L  L  L  L'),
-    (1300, 'The time is one oclock     L'),
-    (1400, 'The time is two oclock     L  L'),
-    (1500, 'The time is three oclock     L  L  L'),
-    (1600, 'The time is four oclock     L  L  L  L'),
-    (1700, 'The time is five oclock     L  L  L  L  L'),
-    (1800, 'The time is six oclock     L  L  L  L  L  L'),
-    (1900, 'The time is seven oclock     L  L  L  L  L  L  L'),
-    (2000, 'The time is eight oclock     L  L  L  L  L  L  L  L'),
-    (2100, 'The time is nine oclock     L  L  L  L  L  L  L  L  L'),
-    (2200, 'The time is ten oclock     L  L  L  L  L  L  L  L  L  L')]
-
-myKOB = kob.KOB(port=PORT, audio=SOUND)
-mySender = morse.Sender(WPM)
-
 def announce(s, kob, sender):
     for c in s:
         code = sender.encode(c)
         kob.sounder(code)
 
-# Announce the current time right now
-now = time.localtime()
-msg = "The time is {hour}:{minute}".format(hour=now.tm_hour, minute=now.tm_min)
-print(msg)
-announce(msg, myKOB, mySender)
+try:
+    import sys
+    import time
+    from pykob import config, kob, morse, log
 
-# Loop, making announcements as configured (every hour, on the hour, during the daytime)
-while True:
-    for m in MSGS:
-        t0 = time.localtime()
-        now = t0.tm_hour*3600 + t0.tm_min*60 + t0.tm_sec  # current time (sec)
-        t1, s = m
-        tMsg = 3600*(t1//100) + 60*(t1%100)  # time to send message
-        dt = tMsg - now  # time to wait
-        if dt > 0:
-            time.sleep(dt)
-            announce(s, myKOB, mySender)
-    time.sleep(24*3600 - now)  # wait until midnight and start over
+    log.log('Starting Clock')
+
+    PORT = config.Port # serial port for KOB interface
+    WPM = config.Speed  # code speed (words per minute)
+    SOUND = config.Sound # whether to enable computer sound for sounder
+    MSGS = [
+        ( 900, 'The time is nine oclock     L  L  L  L  L  L  L  L  L'),
+        (1000, 'The time is ten oclock     L  L  L  L  L  L  L  L  L  L'),
+        (1100, 'The time is eleven oclock     L  L  L  L  L  L  L  L  L  L  L'),
+        (1200, 'The time is twelve oclock     L  L  L  L  L  L  L  L  L  L  L  L'),
+        (1300, 'The time is one oclock     L'),
+        (1400, 'The time is two oclock     L  L'),
+        (1500, 'The time is three oclock     L  L  L'),
+        (1600, 'The time is four oclock     L  L  L  L'),
+        (1700, 'The time is five oclock     L  L  L  L  L'),
+        (1800, 'The time is six oclock     L  L  L  L  L  L'),
+        (1900, 'The time is seven oclock     L  L  L  L  L  L  L'),
+        (2000, 'The time is eight oclock     L  L  L  L  L  L  L  L'),
+        (2100, 'The time is nine oclock     L  L  L  L  L  L  L  L  L'),
+        (2200, 'The time is ten oclock     L  L  L  L  L  L  L  L  L  L')]
+
+    myKOB = kob.KOB(port=PORT, audio=SOUND)
+    mySender = morse.Sender(WPM)
+    
+    # Announce the current time right now
+    now = time.localtime()
+    msg = "The time is {hour}:{minute:02d}".format(hour=now.tm_hour, minute=now.tm_min)
+    print(msg)
+    announce(msg, myKOB, mySender)
+
+    # Loop, making announcements as configured (every hour, on the hour, during the daytime)
+    while True:
+        for m in MSGS:
+            t0 = time.localtime()
+            now = t0.tm_hour*3600 + t0.tm_min*60 + t0.tm_sec  # current time (sec)
+            t1, s = m
+            tMsg = 3600*(t1//100) + 60*(t1%100)  # time to send message
+            dt = tMsg - now  # time to wait
+            if dt > 0:
+                time.sleep(dt)
+                announce(s, myKOB, mySender)
+        time.sleep(24*3600 - now)  # wait until midnight and start over
+except KeyboardInterrupt:
+    print()
+    sys.exit(1)
