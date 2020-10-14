31dac8ffaa1794f2ba352ad047680bfc0dbe60bb
Les Kerr <63867213+leskerr@users.noreply.github.com>
2020-10-14 10:04:31 -0700

    Move SysCheck files to PyKOB directory

--- InstallModules.cmd
+++ InstallModules.cmd
@@ -1,5 +1,6 @@
 :: Windows command file to install the PyAudio and pySerial modules required by PyKOB.
 :: Python 3 must be installed first.
 
-py -m pip install pyaudio
+py -m pip install pipwin 
+py -m pipwin install pyaudio
 py -m pip install pyserial
