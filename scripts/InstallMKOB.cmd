e1b4a7687e75c9660b4a0f4b2cb31331d767c63e
Ed Silky <aesilky@gmail.com>
2020-11-17 01:14:37 -0800

    Check exist of directory

--- scripts/InstallMKOB.cmd
+++ scripts/InstallMKOB.cmd
@@ -1,17 +1,17 @@
 :: Download MKOB.
 
-@echo off
+::@echo off
 echo Installing MKOB...
 
 :: Create new directory and download MKOB
-chdir %HomePath%\Documents
-mkdir MKOB4
+pushd %HomePath%\Documents
+if not exist "%HomePath%\Documents\MKOB4\" mkdir MKOB4
 chdir MKOB4
 powershell (New-Object System.Net.WebClient).DownloadFile('https://github.com/MorseKOB/PyKOB/archive/master.zip','PyKOB.zip')
 
 :: Unzip and remove the download file
 powershell Expand-Archive -Force -LiteralPath PyKOB.zip -DestinationPath .
-del PyKOB.zip
+if exist "PyKOB.zip" del PyKOB.zip
 
 :: Create desktop shortcuts
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%HomePath%\Desktop\MKOB4.lnk');$s.TargetPath='%HomePath%\Documents\MKOB4\PYKOB-master\scripts\MKOB.cmd';$s.WorkingDirectory='%HomePath%\Documents\MKOB4\PYKOB-master\scripts';$s.Save()"
@@ -19,3 +19,4 @@ powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%HomePath%\Deskto
 
 echo Installation complete.
 pause
+popd
