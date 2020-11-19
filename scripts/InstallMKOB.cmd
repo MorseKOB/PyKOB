0d5c1555c950afa138fa8432fbead923e51014e4
Les Kerr <63867213+leskerr@users.noreply.github.com>
2020-11-19 15:50:41 -0800

    Fix desktop shortcut for `MKOB4 Scripts`

--- scripts/InstallMKOB.cmd
+++ scripts/InstallMKOB.cmd
@@ -14,9 +14,8 @@ powershell Expand-Archive -Force -LiteralPath PyKOB.zip -DestinationPath .
 del PyKOB.zip
 
 :: Create desktop shortcuts
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%HomePath%\Desktop\MKOB4.lnk');$s.TargetPath='%HomePath%\Documents\MKOB4\PYKOB-master\scripts\MKOB.cmd';$s.WorkingDirectory='%HomePath%\Documents\MKOB4\PYKOB-master\scripts';$s.Save()"
-::powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%HomePath%\Desktop\MKOB4 Scripts.lnk');$s.TargetPath='c:\Users\Les\Documents\MKOB4\PyKOB-master\scripts';$s.Save()"
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%HomePath%\Desktop\MKOB4 Scripts.lnk');$s.TargetPath='%HomePath%\Documents\MKOB4\PyKOB-master\scripts';$s.Save()"
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut($Home+'\Desktop\MKOB4.lnk');$s.TargetPath=$Home+'\Documents\MKOB4\PYKOB-master\scripts\MKOB.cmd';$s.WorkingDirectory=$Home+'\Documents\MKOB4\PYKOB-master\scripts';$s.Save()"
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut($Home+'\Desktop\MKOB4 Scripts.lnk');$s.TargetPath=$Home+'\Documents\MKOB4\PyKOB-master\scripts';$s.Save()"
 
 echo Installation complete.
 pause
