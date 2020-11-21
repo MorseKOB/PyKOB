f5d99152edffcdf386a931b2041e1fa5dc4700f8
Les Kerr <63867213+leskerr@users.noreply.github.com>
2020-11-21 13:26:28 -0800

    Update installation scripts
    Uncomment `@echo off` in `kInstallMKOB.cmd`.
    Add pip upgrade to `InstallPackages.cmd`.

--- scripts/InstallMKOB.cmd
+++ scripts/InstallMKOB.cmd
@@ -4,14 +4,14 @@
 echo Installing MKOB...
 
 :: Create new directory and download MKOB
-chdir %HomePath%\Documents
-mkdir MKOB4
+pushd %HomePath%\Documents
+if not exist "%HomePath%\Documents\MKOB4\" mkdir MKOB4
 chdir MKOB4
 powershell (New-Object System.Net.WebClient).DownloadFile('https://github.com/MorseKOB/PyKOB/archive/master.zip','PyKOB.zip')
 
 :: Unzip and remove the download file
 powershell Expand-Archive -Force -LiteralPath PyKOB.zip -DestinationPath .
-del PyKOB.zip
+if exist "PyKOB.zip" del PyKOB.zip
 
 :: Create desktop shortcuts
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut($Home+'\Desktop\MKOB4.lnk');$s.TargetPath=$Home+'\Documents\MKOB4\PYKOB-master\scripts\MKOB.cmd';$s.WorkingDirectory=$Home+'\Documents\MKOB4\PYKOB-master\scripts';$s.Save()"
@@ -19,3 +19,4 @@ powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut($Home+'\Desktop\MK
 
 echo Installation complete.
 pause
+popd
